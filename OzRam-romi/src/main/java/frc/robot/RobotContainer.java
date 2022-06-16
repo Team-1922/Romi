7daf5f36e5a5e9897b83c3d779b83c3f8a825f55
schindm <schindm@users.noreply.github.com>
2022-06-15 20:54:55 -0400

    functioning KeyboardDriveCommand

--- OzRam-romi/src/main/java/frc/robot/RobotContainer.java
+++ OzRam-romi/src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,13 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.KeyboardDriveCommand;
 import frc.robot.subsystems.RomiDrivetrain;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -19,10 +22,15 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final RomiDrivetrain m_romiDrivetrain = new RomiDrivetrain();
 
+  private final Joystick m_joystick = new Joystick(0);
+
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_romiDrivetrain);
+  private final KeyboardDriveCommand m_keyboardDriveCommand = 
+    new KeyboardDriveCommand(m_romiDrivetrain, m_joystick);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    m_romiDrivetrain.setDefaultCommand(m_keyboardDriveCommand);
     // Configure the button bindings
     configureButtonBindings();
   }
@@ -33,7 +41,10 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    // new JoystickButton(m_joystick, 1)
+    //   .whenHeld(m_keyboardDriveCommand);
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.

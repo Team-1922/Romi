a420a847ee6eb7fc3e1a10350ec0fe953324d0fd
ESchindewolf <schindewolf.evan@gmail.com>
2024-09-07 15:10:07 -0400

    add keyboard drive command

--- OzRam-Romi/src/main/java/frc/robot/RobotContainer.java
+++ OzRam-Romi/src/main/java/frc/robot/RobotContainer.java
@@ -4,8 +4,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.KeyboardDriveCommand;
 import frc.robot.subsystems.RomiDrivetrain;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -18,11 +20,15 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final RomiDrivetrain m_romiDrivetrain = new RomiDrivetrain();
+  private final Joystick m_joystick = new Joystick(0);
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_romiDrivetrain);
+  private final KeyboardDriveCommand m_keyboardDriveCommand = 
+    new KeyboardDriveCommand(m_romiDrivetrain, m_joystick);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    m_romiDrivetrain.setDefaultCommand(m_keyboardDriveCommand);
     // Configure the button bindings
     configureButtonBindings();
   }
